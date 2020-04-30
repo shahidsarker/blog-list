9abaeed416e183749feefaf608fd098eef92a009
Shahid <shahid.sarker@gmail.com>
2020-04-30 19:14:21 -0400

    refactor mongodb connection and logging

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const config = require("./utils/config");
-const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => logger.info("connected to MongoDB"))
+  .catch((error) =>
+    logger.error("error connecting to MongoDB:", error.message)
+  );
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 module.exports = app;
