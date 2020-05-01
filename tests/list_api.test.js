ee99cb24942be375478cc2f300a979a5b228984e
Shahid <shahid.sarker@gmail.com>
2020-05-01 16:31:34 -0400

    sample data for testing

--- tests/list_api.test.js
+++ tests/list_api.test.js
@@ -1,14 +1,42 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
 const api = supertest(app);
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+];
 
 test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test("there are two blogs", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(2);
+});
+
+test("first blog is about react patterns", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body[0].title).toBe("React patterns");
 });
 
 afterAll(() => {
