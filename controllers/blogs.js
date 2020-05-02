a3ac3e161967eb0f4582f854e44426a78e3e3ca0
Shahid <shahid.sarker@gmail.com>
2020-05-01 23:05:29 -0400

    test for blog missing likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,8 +7,15 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (req, response) => {
+  const body = req.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes === undefined ? 0 : req.body,
+  });
 
   const result = await blog.save();
   response.status(201).json(result);
