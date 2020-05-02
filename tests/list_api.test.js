50c652e06bb4e9f09d729b196c04a7235096b7df
Shahid <shahid.sarker@gmail.com>
2020-05-01 22:55:59 -0400

    test for valid blog add

--- tests/list_api.test.js
+++ tests/list_api.test.js
@@ -1,29 +1,30 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
 
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url:
-      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-];
+// const initialBlogs = [
+//   {
+//     title: "React patterns",
+//     author: "Michael Chan",
+//     url: "https://reactpatterns.com/",
+//     likes: 7,
+//   },
+//   {
+//     title: "Go To Statement Considered Harmful",
+//     author: "Edsger W. Dijkstra",
+//     url:
+//       "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+//     likes: 5,
+//   },
+// ];
 
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  for (let blog of initialBlogs) {
+  for (let blog of helper.initialBlogs) {
     let blogObject = new Blog(blog);
     await blogObject.save();
   }
@@ -39,7 +40,7 @@ test("blogs are returned as json", async () => {
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
 
-  expect(response.body).toHaveLength(initialBlogs.length);
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
 test("a specific blog is within the returned blogs", async () => {
@@ -54,6 +55,27 @@ test("blog has id not _id", async () => {
   const response = await api.get("/api/blogs");
 
   expect(response.body[0].id).toBeDefined();
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const title = blogsAtEnd.map((r) => r.title);
+  expect(title).toContain("Canonical string reduction");
 });
 
 afterAll(() => {
