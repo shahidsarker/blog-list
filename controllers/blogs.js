59e97653fbfb92a46057c5d02385137e0df5b1c3
Shahid <shahid.sarker@gmail.com>
2020-05-03 03:04:38 -0400

    Implement route to update likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,4 +30,17 @@ blogsRouter.delete("/:id", async (req, res) => {
   res.send(204).end();
 });
 
+blogsRouter.put("/:id", async (req, res) => {
+  const body = req.body;
+
+  const blog = {
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+    new: true,
+  });
+  res.json(updatedBlog.toJSON());
+});
+
 module.exports = blogsRouter;
