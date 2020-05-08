7fb6250a029787bda8c18f8e3d887df0b648028c
Shahid <shahid.sarker@gmail.com>
2020-05-07 21:10:12 -0400

    errorHandler handles jwt errors

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,7 +18,9 @@ const errorHandler = (error, req, res, next) => {
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    return res.status(400).send({ error: error.message });
+    return res.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return res.status(401).json({ error: "invalid token" });
   }
 
   next(error);
