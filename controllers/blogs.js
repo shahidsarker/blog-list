fc42669577c343e895f1a580bfa14b01a0f6a14d
Shahid <shahid.sarker@gmail.com>
2020-05-11 19:54:37 -0400

    Delete requires token first attempt

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,30 +3,22 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-blogsRouter.get("/", async (request, response) => {
+blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
-  console.log(request.token);
-  response.json(blogs);
+  console.log(req.token);
+  res.json(blogs);
 });
 
-// const getTokenFrom = (req) => {
-//   const authorization = req.get("authorization");
-//   if (authorization && authorization.toLowerCase().startsWith("bearer "))
-//     return authorization.substring(7);
-//   return null;
-// };
-
-blogsRouter.post("/", async (req, response) => {
+blogsRouter.post("/", async (req, res) => {
   const body = req.body;
 
   if (!body.title || !body.url) {
-    return response.status(400).end();
+    return res.status(400).end();
   }
 
-  // const token = getTokenFrom(req);
   const decodedToken = jwt.verify(req.token, process.env.SECRET);
   if (!req.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    return res.status(401).json({ error: "token missing or invalid" });
   }
 
   const user = await User.findById(decodedToken.id);
@@ -43,12 +35,23 @@ blogsRouter.post("/", async (req, response) => {
   user.blogs = user.blogs.concat(savedBlog._id);
   await user.save();
 
-  response.status(201).json(savedBlog.toJSON());
+  res.status(201).json(savedBlog.toJSON());
 });
 
 blogsRouter.delete("/:id", async (req, res) => {
-  await Blog.findByIdAndRemove(req.params.id);
-  res.send(204).end();
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+  if (!req.token || !decodedToken.id) {
+    return res.status(401).json({ error: "token missing or invalid" });
+  }
+
+  const loggedInId = decodedToken.id;
+  const blog = await Blog.findById(req.params.id);
+  if (loggedInId.toString() === blog.user.toString()) {
+    await Blog.findByIdAndRemove(blog._id);
+    res.send(204).end();
+  } else {
+    return res.status(401).json({ error: "invalid user" });
+  }
 });
 
 blogsRouter.put("/:id", async (req, res) => {
